dcd09242630247e34362d92be9e0d61846bdd415
Firdaus-Abd-Rahman <firdaus.rahman.777@gmail.com>
2019-04-25 11:01:31 +0800

    added comments for future reference

--- caesars-cipher.js
+++ caesars-cipher.js
@@ -1,13 +1,21 @@
 function rot13(str) {
-  return str.split('').map.call(str, function(char) {
+  //split str into a char string
+  return str.split('')
+  //iterate over each char in the array
+  .map.call(str, function(char) {
+    //convert char to a char code
     let x = char.charCodeAt(0);
-      if (x < 65 || x > 90) {
-        return String.fromCharCode(x);
+    //check if char lies between A-Z
+    if (x < 65 || x > 90) {
+       //return un-converted char
+       return String.fromCharCode(x);
+        
+      //if char code is less than 78, shift forward 13 places
       }else if (x < 78) {
         return String.fromCharCode(x + 13);
-      }
+      }//otherwise, shift the char 13 places backward
       return String.fromCharCode(x - 13);
-      }).join(''); 
+      }).join(''); //rejoin the array into a string
     }
 
 console.log(rot13("SERR PBQR PNZC"));
